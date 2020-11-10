74a73b2dcd5755e59b0b493e1094290d070b8f9d
rafik mrabet <mrabetr@hotmail.com>
2020-11-10 15:18:43 +0000

    updated without css

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :articles
+  # # view all
+  # get '/articles', to: 'articles#index'
+  # # create new
+  # get 'articles/new', to: 'articles#new', as: :new_article
+  # post 'articles', to: 'articles#create'
+  # # view one
+  # get 'articles/:id', to: 'articles#show', as: :task
+  # # update
+  # get 'articles/:id/edit', to: 'articles#edit', as: :edit_article
+  # patch 'articles/:id', to: 'articles#update'
+  # # delete
+  # delete 'articles/:id', to: 'articles#destroy'
 end
