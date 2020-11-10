50c3dd02056a6a94a448986c803ee3f33d2bd65b
rafik mrabet <mrabetr@hotmail.com>
2020-11-10 19:02:26 +0000

    updated with styling

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,39 +1,49 @@
 class ArticlesController < ApplicationController
+  before_action :new_article, only: [:new, :create]
+  before_action :find_article, only: [:show, :edit, :update, :destroy]
+
   def index
     @articles = Article.all
   end
 
-  def show
-    @article = Article.find(params[:id])
-  end
+  # @article = Article.find(params[:id])
+  def show; end
 
-  def new
-    @article = Article.new
-  end
+  # @article = Article.new
+  def new; end
 
   def create
-    @article = Article.new(article_params)
-    @article.save
-    redirect_to article_path(@article)
+    # @article = Article.new(article_params)
+    # @article.save
+    # replaced the above two lines after adding new_article
+    @article.update(article_params)
+    redirect_to articles_path
   end
 
-  def edit
-    @article = Article.find(params[:id])
-  end
+  # @article = Article.find(params[:id])
+  def edit; end
 
   def update
-    @article = Article.find(params[:id])
+    # @article = Article.find(params[:id])
     @article.update(article_params)
     redirect_to article_path(@article)
   end
 
   def destroy
-    @article = Article.find(params[:id])
+    # @article = Article.find(params[:id])
     @article.destroy
     redirect_to articles_path
   end
 
   private
+
+  def new_article
+    @article = Article.new
+  end
+
+  def find_article
+    @article = Article.find(params[:id])
+  end
 
   def article_params
     params.require(:article).permit(:title, :content)
